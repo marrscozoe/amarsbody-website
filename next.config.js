c5296c82da26a4cb6e2c7f41fd8b31f1a080e42c
Zoe (AI) <zoe@openclaw>
2026-09-11 18:26:09 -0500

    fix: disable turbopack in build, externalize nodemailer, add it to deps

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable Turbopack - use Webpack bundler instead
-  // Turbopack has issues resolving certain packages like nodemailer
-  experimental: {
-    turbo: false,
-  },
+  // Externalize nodemailer so Turbopack doesn't try to bundle it
   serverExternalPackages: ['nodemailer'],
 };
 
